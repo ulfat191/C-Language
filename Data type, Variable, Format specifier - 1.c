f0f0c70a2605e45c8b51a9bc672286f5efdbd82a
Abdul Mukit <103219520+abdulmukitu@users.noreply.github.com>
2022-04-09 23:11:33 +0600

    learning-variables

---
+++ Data type, Variable, Format specifier - 1.c
@@ -0,0 +1,16 @@
+/*
+An integer variable num that contains 100,
+Write a prgram that prints an integer number.
+*/
+#include<stdio.h>
+int main ()
+{
+    int num=100;
+    printf("Number is %d",num);
+    return 0;
+}
+/*
+Here, int is a keyword of data type,
+printf() is a library function,
+num is a variable
+*/
